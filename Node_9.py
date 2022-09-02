14fe0c49f70507db720a0fb001397374024ce7f8
kmk7991 <kmk7102@naver.com>
2022-09-02 09:40:09 +0000

    mission complete

---
+++ Node_9.py
@@ -0,0 +1,6 @@
+
+import matplotlib.pyplot as plt
+plt.bar(['americano', 'dolce latte', 'caffe \nmocha', 'caramel \nmacchiato','caffe latte'],[4100, 5600, 5100, 5600, 4600])
+plt.title('Mission Complete')
+plt.ylabel('price (won))')
+plt.show()
